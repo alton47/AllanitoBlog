79980f6754b022a3b369b9b7931fb054b518064f
Allan <altonallan47@gmail.com>
2023-03-15 18:46:57 +0100

    Add an eNdpoiNt for submitComment

--- services/index.js
+++ services/index.js
@@ -155,3 +155,18 @@ export const getRecentPosts = async () => {
   
 
 
+
+  //subMit Comment
+  export const submitComment = async (obj) => {
+    const result = await fetch('/api/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    });
+  
+    return result.json();
+  };
+
+
