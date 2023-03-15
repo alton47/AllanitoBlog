768553e8ff9e55257348a54c47031d9a9cca44e5
Allan <altonallan47@gmail.com>
2023-03-15 20:44:26 +0100

    Add EndPoint for getComments

--- services/index.js
+++ services/index.js
@@ -170,3 +170,23 @@ export const getRecentPosts = async () => {
   };
 
 
+
+
+  //getComments
+export const getComments = async (slug) => {
+    const query = gql`
+      query GetComments($slug:String!) {
+        comments(where: {post: {slug:$slug}}){
+          name
+          createdAt
+          comment
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.comments;
+  };
+  
+
