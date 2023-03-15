1c08c12a4537d0adb0969b5210c6d4c53cc5ddba
Allan <altonallan47@gmail.com>
2023-03-15 20:41:35 +0100

    Update Comments.jsx

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import moment from 'moment';
+import moment from 'moment'
 {/*import parse from 'html-react-parser';
 
 import { getComments } from '../services';
