1a8b353eb188b6c72c003aa767d5291ed6cac363
Allan <altonallan47@gmail.com>
2023-03-15 14:34:35 +0100

    add HEader.jsx font

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,13 +10,13 @@ const Header = () => {
               <div className="border-b w-full inline-block border-blue-400 py-8">
               <div className="md:float-left block">
               <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Allanito</span>
+            <span className="cursor-pointer font-bold text-4xl text-black">Allanito</span>
           </Link>
                </div>
                <div className="hidden md:float-left md:contents">
                {categories.map((category, index) => (
             <Link key={index} href={`/category/${category.slug}`}>
-                <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                <span className="md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer">
                     {category.name}</span>
                     </Link>
           ))}
