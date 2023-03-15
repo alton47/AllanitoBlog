12d7945713074f24d1c3aa9ad18c577ec51a8c63
Allan <altonallan47@gmail.com>
2023-03-15 12:14:17 +0100

    Update _app.tsx
    Link the Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
+import React from 'react';
 import type { AppProps } from 'next/app'
+import { Layout } from '../components';
+import 'tailwindcss/tailwind.css';
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp
+
+{/* 
+
+
+function MyApp({ Component, pageProps }) {
+  
+}
+
+export default MyApp;
+  */}
